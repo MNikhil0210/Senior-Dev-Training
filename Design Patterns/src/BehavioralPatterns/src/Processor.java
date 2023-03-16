407396719bbe416594c0156a777a5070c09df851
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2023-03-16 12:32:02 +0530

    feat: implemented chain of responsibility pattern

---
+++ Design Patterns/src/BehavioralPatterns/src/Processor.java
@@ -0,0 +1,13 @@
+abstract class Processor
+{
+    private final Processor nextProcessor;
+
+    public Processor(Processor nextProcessor){
+        this.nextProcessor = nextProcessor;
+    };
+
+    public void process(Number request){
+        if(nextProcessor != null)
+            nextProcessor.process(request);
+    };
+}
